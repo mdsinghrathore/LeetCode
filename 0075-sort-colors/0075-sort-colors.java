0cbfb784942b81e65bedbaec458d8c243ac90f0f
MANVENDERDEEP SINGH RATHORE <mdsinghrathore190@gmail.com>
2026-09-19 22:22:15 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.17%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,25 @@
 class Solution {
+    public static void swap(int[] nums,int a,int b)
+    {
+        int t=nums[a];
+        nums[a]=nums[b];
+        nums[b]=t;
+    }
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==1)
+            mid++;
+            else if(nums[mid]==0){
+            swap(nums,low,mid);
+            low++;
+            mid++;}
+            else
+            {
+                swap(nums,high,mid);
+                high--;
+            }
+        }
     }
 }
